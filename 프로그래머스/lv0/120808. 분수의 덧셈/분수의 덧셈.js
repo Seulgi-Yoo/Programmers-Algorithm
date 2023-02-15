27ef66dc0d27f341b8bc9b2faba27a6acded5f96
Seulgi-Yoo <119473025+Seulgi-Yoo@users.noreply.github.com>
2023-02-15 16:02:42 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -0,0 +1,10 @@
+function solution(numer1, denom1, numer2, denom2) {
+    // 분수의 합: a/b + c/d = (a*d)+(b*c)/b*d
+    // 기약 분수: 최대공약수로 분자와 분모를 각각 나누어주기
+    const 분자 = (numer1 * denom2) + (numer2 * denom1)
+    const 분모 = denom1 * denom2
+    // 최대공약수 구하기
+    const getGCD = (a, b) => (b === 0 ? a : getGCD(b, a % b));
+    const 최대공약수 = getGCD(분모, 분자);
+    return [분자/최대공약수, 분모/최대공약수];
+}
