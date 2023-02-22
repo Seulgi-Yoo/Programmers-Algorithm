6ce3d7d79d0dc1a15b8134a8577c8fb551298845
Seulgi-Yoo <119473025+Seulgi-Yoo@users.noreply.github.com>
2023-02-22 13:46:07 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.js
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -1,7 +1,9 @@
 function solution(strlist) {
-    let answer = [];
-    for (let el of strlist) {
-        answer.push(el.length)
-    }
-    return answer;
+    // let answer = [];
+    // for (let el of strlist) {
+    //     answer.push(el.length)
+    // }
+    // return answer;
+    
+    return strlist.map(el => el.length)
 }
