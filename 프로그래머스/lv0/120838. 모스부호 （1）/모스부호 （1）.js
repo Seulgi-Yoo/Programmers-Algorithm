2a5e8da4cffaffb5830ed162f1ea66f5ff5017fd
Seulgi-Yoo <119473025+Seulgi-Yoo@users.noreply.github.com>
2023-03-01 21:34:37 +0900

    [level 0] Title: 모스부호 (1), Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -7,9 +7,11 @@ function solution(letter) {
     '-.--':'y','--..':'z'
 }
     const morseArr = letter.split(' '); // letter를 공백 기준으로 잘라서 배열로 변환
-    let result = '';
-    for (let i = 0; i < morseArr.length; i++) {
-        result += morse[morseArr[i]];; // 해독할 모스부호를 키 값으로 가지고 있는 요소 찾아서 value 더해주기
-     }
-    return result;
+    // let result = '';
+    // for (let i = 0; i < morseArr.length; i++) {
+    //     result += morse[morseArr[i]];; // 해독할 모스부호를 키 값으로 가지고 있는 요소 찾아서 value 더해주기
+    //  }
+    // return result;
+    return morseArr.map(x=> morse[x]).join('')
+    
 }
